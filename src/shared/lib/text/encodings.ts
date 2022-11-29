888d685c6c29517342079649c4ec15b7df688d38
Chris Maltby <chris.maltby@gmail.com>
2022-11-29 22:23:11 +0000

    Remove unused encodings functionality (was replaced with per font encoding files)

--- src/shared/lib/text/encodings.ts
+++ src/shared/lib/text/encodings.ts
@@ -1,37 +1,3 @@
-// import fs from "fs";
-// import glob from "glob";
-// import Path from "path";
-// import { localesRoot } from "../../consts";
-
-interface EncodingData {
-  name: string;
-  mapping: Record<string, number | undefined>;
-}
-
-type EncodingDef = EncodingData & {
-  id: string;
-};
-
-// const encodingsPath = `${localesRoot}/encodings/*.json`;
-
-export const encodings: EncodingDef[] =
-  []; /*glob.sync(encodingsPath).map((path) => {
-  try {
-    const data = JSON.parse(fs.readFileSync(path, "utf8"));
-    return {
-      id: Path.basename(path, ".json"),
-      ...data,
-    };
-  } catch (e) {
-    return {
-      id: Path.basename(path, ".json"),
-      name: String(e),
-      mapping: {},
-    };
-  }
-});*/
-console.warn("Handle loading encodings");
-
 export const encodeChar = (
   char: string,
   mapping?: Record<string, number>
