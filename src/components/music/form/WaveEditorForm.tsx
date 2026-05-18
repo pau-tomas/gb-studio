6b1522e1caef5afcb92f4b11af6238f4b1ecb5b2
Pau Tomàs <pau.tomas@gmail.com>
2026-05-18 19:58:41 +0100

    Use theme for music editor graphs and previews

--- src/components/music/form/WaveEditorForm.tsx
+++ src/components/music/form/WaveEditorForm.tsx
@@ -76,7 +76,8 @@ export const WaveEditorForm = ({ waveId, onChange }: WaveEditorFormProps) => {
       return;
     }
 
-    const defaultColor = themeContext?.colors.highlight ?? "black";
+    const defaultColor = themeContext?.colors.highlight ?? "#fff";
+    const backgroundColor = themeContext?.colors.input.background ?? "#000";
 
     const getLayout = () => {
       const rect = canvas.getBoundingClientRect();
@@ -115,7 +116,7 @@ export const WaveEditorForm = ({ waveId, onChange }: WaveEditorFormProps) => {
     const clear = () => {
       const { width, height } = getLayout();
       ctx.clearRect(0, 0, width, height);
-      ctx.fillStyle = "#000";
+      ctx.fillStyle = backgroundColor;
       ctx.fillRect(0, 0, width, height);
     };
 
@@ -280,7 +281,14 @@ export const WaveEditorForm = ({ waveId, onChange }: WaveEditorFormProps) => {
       window.removeEventListener("touchcancel", handleTouchEnd);
       window.removeEventListener("resize", handleResize);
     };
-  }, [onEditWave, songWave, themeContext?.colors.highlight, wavesLength]);
+  }, [
+    onEditWave,
+    songWave,
+    themeContext?.colors.background,
+    themeContext?.colors.highlight,
+    themeContext?.colors.input.background,
+    wavesLength,
+  ]);
 
   return (
     <>
--- src/components/music/sidebar/InstrumentEnvelopePreview.tsx
+++ src/components/music/sidebar/InstrumentEnvelopePreview.tsx
@@ -51,14 +51,16 @@ export const InstrumentEnvelopePreview = ({
     const secLength = length === null ? 1 : length / 256;
     const centerLineY = height - PADDING - (7 / 15) * drawHeight;
 
-    const defaultColor = themeContext.colors.highlight;
+    const defaultColor = themeContext?.colors.highlight ?? "#fff";
+    const backgroundColor = themeContext?.colors.input.background ?? "#000";
+    const gridColor = themeContext?.colors.input.border ?? "#333";
 
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = backgroundColor;
     ctx.fillRect(0, 0, width, height);
 
     // center line
     ctx.beginPath();
-    ctx.strokeStyle = "#333";
+    ctx.strokeStyle = gridColor;
     ctx.lineWidth = 1;
     ctx.moveTo(0, Math.round(centerLineY) + 0.5);
     ctx.lineTo(width, Math.round(centerLineY) + 0.5);
